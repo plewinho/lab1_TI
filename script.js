3c7021091d356830660e65c14b0afa2a1e265db7
plewinho <plewinho@gmail.com>
2024-04-11 22:29:17 +0200

    2

--- script.js
+++ script.js
@@ -54,6 +54,14 @@ setInterval(() => {
 
 barIcon.addEventListener("click", showMenu);
 
+window.addEventListener("resize", function () {
+  if (window.innerWidth > 600) {
+    document.querySelector(".links").style.display = "block";
+  } else {
+    document.querySelector(".links").style.display = "none";
+  }
+});
+
 // ----------
 
 const AddInput = document.querySelector(".AdditionInput");
@@ -66,7 +74,7 @@ const divA = document.getElementById("divA");
 const divB = document.getElementById("divB");
 const quaA = document.getElementById("quaA");
 const quaB = document.getElementById("quaB");
-const divC = document.getElementById("divC");
+const quaC = document.getElementById("quaC");
 const history = document.querySelector(".history");
 
 const calculate = document.querySelector(".oblicz");
@@ -90,31 +98,67 @@ function showInput() {
   }
 }
 const add = () => {
-  let x = parseFloat(addA.value) + parseFloat(addB.value);
-  let y = `${parseFloat(addA.value)} + ${parseFloat(addB.value)} = ${x}`;
-
-  showHistory(x, y);
+  let a = parseFloat(addA.value);
+  let b = parseFloat(addB.value);
+  let x = a + b;
+  let operation = `${a} + ${b} = ${x}`;
+  if (isNaN(a) || isNaN(b)) {
+    showHistory("+ = x", "ERROR(empty input)");
+  } else {
+    showHistory(operation, x.toFixed(2));
+  }
   clearInputs();
 };
 
 const divide = () => {
-  let x = parseFloat(divA.value) / parseFloat(divB.value);
-  let y = `${parseFloat(divA.value)} / ${parseFloat(divB.value)} = ${x}`;
-  showHistory(x, y);
+  let a = parseFloat(divA.value);
+  let b = parseFloat(divB.value);
+  let x = a / b;
+  let operation = `${a} / ${b} = ${x}`;
+  if (isNaN(a) || isNaN(b)) {
+    showHistory("/ = x", "ERROR(empty input)");
+  } else if (b == 0) {
+    showHistory(`${a} / ${b} = x`, `ERROR(b=${b})`);
+  } else {
+    showHistory(operation, x.toFixed(2));
+  }
   clearInputs();
 };
 
-const delta = () => {
-  let x = 
+const quadratic = () => {
+  let a = parseFloat(quaA.value);
+  let b = parseFloat(quaB.value);
+  let c = parseFloat(quaC.value);
+  let delta = b * b - 4 * a * c;
+  if (isNaN(a) || isNaN(b) || isNaN(c)) {
+    showHistory(`x^2 + x + = x`, `ERROR(empty input)`);
+  } else if (delta < 0) {
+    showHistory(`${a} x^2 + ${b} x + ${c} = x`, `ERROR(Δ<0)`);
+  } else if (delta === 0) {
+    let x = -b / (2 * a);
+    showHistory(`${a} x^2 + ${b} x + ${c} = x`, `x = ${x.toFixed(2)}`);
+  } else {
+    let x1 = (-b - Math.sqrt(delta)) / (2 * a);
+    let x2 = (-b + Math.sqrt(delta)) / (2 * a);
+    showHistory(
+      `${a} x^2 + ${b} x + ${c} = x`,
+      `<div><p id="pl">x1 = ${x1.toFixed(2)}</p> <p id="pr">  x2 = ${x2.toFixed(
+        2
+      )} </p></div>`
+    );
+  }
 
-
-}
+  clearInputs();
+};
 
 const clearInputs = () => {
   addA.value = "";
   addB.value = "";
   divA.value = "";
   divB.value = "";
+  quaA.value = "";
+  quaB.value = "";
+  quaC.value = "";
 };
 
 calculate.addEventListener("click", () => {
@@ -123,23 +167,19 @@ calculate.addEventListener("click", () => {
   } else if (document.getElementById("div").checked) {
     divide();
   } else if (document.getElementById("que").checked) {
+    quadratic();
   } else {
-    alert("Wybierz operacje!!");
+    showHistory("---", "ERROR");
   }
 });
 
-clear.addEventListener("click", () => {
-  history.textContent = "";
-  num = 1;
-});
-
-const showHistory = (x, y) => {
+const showHistory = (oper, x) => {
   const id = document.createElement("li");
   const operations = document.createElement("li");
   const wynik = document.createElement("li");
   id.innerHTML = `${num}.`;
-  operations.innerHTML = y;
   wynik.innerHTML = x;
+  operations.innerHTML = oper;
   history.appendChild(id);
   history.appendChild(operations);
 
@@ -147,3 +187,8 @@ const showHistory = (x, y) => {
 
   num++;
 };
+
+clear.addEventListener("click", () => {
+  history.textContent = "";
+  num = 1;
+});
